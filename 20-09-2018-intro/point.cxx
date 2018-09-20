4edddcc7ecb4896a7cede337eb310a146a443961
Martin Jordanov <xxm0703@abv.bg>
2018-09-20 10:38:34 +0300

    Point print

--- 20-09-2018-intro/point.cxx
+++ 20-09-2018-intro/point.cxx
@@ -1,5 +1,6 @@
+#include<iostream>
 
-
+using namespace std;
 class Point {
 	int x_, y_;
 
@@ -9,10 +10,16 @@ public:
 		x_ = x;
 		y_ = y;
 	}
+
+	void print() {
+		cout << '(' << x_ << ", " << y_ << ')' << endl;
+	}
 };
 
 int main(){
-	Point p;
-
+	Point p1(10,15);
+	Point p2(-1, 20);
+	p1.print();
+	p2.print();
 	return 0;
 }
