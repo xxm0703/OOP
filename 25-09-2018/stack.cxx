9352522cd57a07e946528f225ac5d8ac07703372
Martin Jordanov <xxm0703@abv.bg>
2018-09-25 12:30:26 +0300

    Implementing Stack

---
+++ 25-09-2018/stack.cxx
@@ -0,0 +1,83 @@
+
+#include<iostream>
+using namespace std;
+
+#define STACK_SIZE 10
+
+class Stack {
+    int data_[STACK_SIZE];
+    int top_;
+public:
+    Stack() {
+        top_ = 0;
+    }
+
+    bool empty() {
+        return !top_;
+    }
+
+    bool full() {
+        return top_ == STACK_SIZE;
+    }
+
+    int pop() {
+        if(empty()) {
+            cout << "OOPPSSS! stack is empty..." << endl;
+            return 0;
+        }
+        return data_[--top_];
+    }
+
+    void push(int value) {
+        if(full()){ 
+            cout << "OOPPSSS! stack is empty..." << endl;
+            return;
+        }
+        data_[top_++] = value;
+    }
+
+};
+
+void unbalanced() {
+	cout << "Unbalanced..." << endl;
+}
+
+int main() {
+    Stack st;  
+	char ch;
+	while(cin.get(ch)) {
+		switch(ch) {
+			case ')':
+				if(st.pop() != '(') {
+					unbalanced();
+					return 0;
+				}
+				break;
+
+			case '}':
+				if(st.pop() != '{') {
+					unbalanced();
+					return 0;
+				}
+				break;
+
+			case ']':
+				if(st.pop() != '[') {
+					unbalanced();
+					return 0;
+				}
+				break;
+			
+			default:
+				if(ch == '(' || ch == '[' || ch == '{')
+					st.push(ch);
+				break;
+			}
+	}
+	if(!st.empty())
+		unbalanced();
+	else
+		cout << "Everything is OK!" << endl;  
+    return 0;
+}
+
