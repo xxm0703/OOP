79eb5025a988748ab4b6d80bd028b4ab6097c0b6
Martin_Jordanov <martistj@gmail.com>
2018-09-29 16:06:51 +0300

    Reading and pushing

--- 27-09-2018-practice/example.cxx
+++ 27-09-2018-practice/example.cxx
@@ -1,15 +1,27 @@
 #include<iostream>
 #include<fstream>
+#include<vector>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    ofstream f("myfile.txt");
-    if(!f.is_open()) {
-        return 0;
+
+	ifstream cin("myfile.txt");
+
+	if(!cin.is_open()) {
+		printf("Not working\n");
+		return 0;
     }
+	std::vector<float> v;
+	float a;
+	while (true) {
+		cin >> a;
+		if (!cin)
+			break;
+		v.push_back(a);
+	}
+	std::cout << v.at(0) << '\n';
 
-
-    f.close();
+    cin.close();
+	return 0;
 }
-
