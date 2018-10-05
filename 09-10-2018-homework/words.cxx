27a6cb1e7c7ffe41373bf79c87ddf67443f8b006
Martin Jordanov <xxm0703@abv.bg>
2018-10-05 23:28:54 +0300

    [HW] Set Up

---
+++ 09-10-2018-homework/words.cxx
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Text {
+	vector
+
+};
+
+int main() {
+	string word;
+	while(true) {
+		cin >> word
+		if (!strcmp(word, "END")) {
+
+		}
+	}
+	return 0;
+}
