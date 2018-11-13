02fadef8f82b03d78f3a4efee0377a7430e8d3f7
Martin Jordanov <xxm0703@abv.bg>
2018-11-13 14:06:08 +0200

    [Fix] Fix inheritance

--- 06-11-2018-const_iterator/vector.cxx
+++ 06-11-2018-const_iterator/vector.cxx
@@ -22,6 +22,52 @@ class Vector {
         return size_ == STACK_STEP;
     }
 
+	class base_iterator {
+		friend Vector;
+	protected:
+		int *element_;
+
+		base_iterator(int *element) :
+			element_(element)
+		{}
+
+		base_iterator(const base_iterator &it) :
+			element_(it.element_)
+		{}
+
+	public:
+		base_iterator &operator++() {
+			element_++;
+			return *this;
+		}
+
+		base_iterator operator++(int) {
+			base_iterator tmp(*this);
+			element_++;
+			return tmp;
+		}
+
+		base_iterator &operator--() {
+			element_--;
+			return *this;
+		}
+
+		base_iterator operator--(int) {
+			base_iterator tmp(*this);
+			element_--;
+			return tmp;
+		}
+
+		bool operator==(const base_iterator &other) {
+			return element_ == other.element_;
+		}
+
+		bool operator!=(const base_iterator &other) {
+			return !operator==(other);
+		}
+
+	};
+
 public:
 	Vector() : size_(0),
 	capacity_(STACK_STEP),
@@ -72,57 +118,23 @@ public:
 		return capacity_;
 	}
 
-	class base_iterator {
-        friend Vector;
-    protected:
-        int *element_;
-
-        base_iterator(int *element) :
-            element_(element)
-        {}
-
-        base_iterator(const base_iterator &it) :
-            element_(it.element_)
-        {}
-
-	public:
-		base_iterator &operator++() {
-			element_++;
-			return *this;
-		}
-
-		base_iterator operator++(int) {
-			base_iterator tmp(*this);
-			element_++;
-			return tmp;
-		}
-
-		base_iterator &operator--() {
-			element_--;
-			return *this;
-		}
-
-		base_iterator operator--(int) {
-			base_iterator tmp(*this);
-			element_--;
-			return tmp;
-		}
-
-		bool operator==(const base_iterator &other) {
-			return element_ == other.element_;
-		}
-
-		bool operator!=(const base_iterator &other) {
-			return !operator==(other);
-		}
-
-	};
-
     class iterator : public base_iterator {
         friend Vector;
 
         iterator(int *element) :
             base_iterator(element)
+        {}
+    public:
+        int &operator*() {
+            return *element_;
+        }
+    };
+
+    class reverse_iterator : public iterator {
+        friend Vector;
+
+        reverse_iterator(int *element) :
+            iterator(element)
         {}
     public:
         base_iterator operator++() {
@@ -136,21 +148,9 @@ public:
         base_iterator operator--() {
             return base_iterator::operator++();
         }
-        
+
         base_iterator operator--(int) {
             return base_iterator::operator--(5);
-        }
-    };
-
-    class reverse_iterator : public iterator {
-        friend Vector;
-
-        reverse_iterator(int *element) :
-            iterator(element)
-        {}
-    public:
-        int &operator*() {
-            return *element_;
         }
     };
 
@@ -166,6 +166,30 @@ public:
 		}
 	};
 
+	class const_reverse_iterator : public const_iterator {
+		friend Vector;
+
+		const_reverse_iterator(int *element) :
+			const_iterator(element)
+		{}
+	public:
+		base_iterator operator++() {
+			return base_iterator::operator--();
+		}
+
+		base_iterator operator++(int) {
+			return base_iterator::operator--(5);
+		}
+
+		base_iterator operator--() {
+			return base_iterator::operator++();
+		}
+
+		base_iterator operator--(int) {
+			return base_iterator::operator--(5);
+		}
+	};
+
     iterator begin() {
         return iterator(buffer_);
     }
@@ -173,6 +197,30 @@ public:
     iterator end() {
         return iterator(buffer_+size_);
     }
+
+	reverse_iterator rbegin() {
+		return reverse_iterator(buffer_ + size_ - 1);
+	}
+
+	reverse_iterator rend() {
+		return reverse_iterator(buffer_ - 1);
+	}
+
+	const_iterator begin() const {
+		return const_iterator(buffer_);
+	}
+
+	const_iterator end() const {
+		return const_iterator(buffer_+size_);
+	}
+
+	const_reverse_iterator rbegin() const {
+		return const_reverse_iterator(buffer_ + size_ - 1);
+	}
+
+	const_reverse_iterator rend() const {
+		return const_reverse_iterator(buffer_ - 1);
+	}
 
 	int &back() {
 		return buffer_[size_];
@@ -220,7 +268,15 @@ int main() {
 	cout << st.size() << endl;
 	st.push_back(33);
 	cout << st.back() << endl;
-	Vector st1 = st;
+	const Vector st1 = st;
 	cout << st1.back() << endl;
+	cout << "Start loop..." << endl;
+	for (Vector::reverse_iterator it = st.rbegin(); it != st.rend(); it++) {
+		cout << *it << endl;
+	}
+	// cout << "Start second loop..." << endl;
+	// for (Vector::reverse_iterator it = st.rbegin(); it != st.rend(); it++) {
+	// 	cout << *it << endl;
+	// }
 	return 0;
 }
