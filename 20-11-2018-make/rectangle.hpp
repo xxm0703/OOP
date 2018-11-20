3117fd05dd5666b9684fe1b852eb7c5ef377dd9f
Martin Jordanov <xxm0703@abv.bg>
2018-11-20 14:19:48 +0200

    [Add] Some headers

--- 20-11-2018-make/rectangle.hpp
+++ 20-11-2018-make/rectangle.hpp
@@ -1,14 +1,18 @@
+#include <iostream>
 #include "point.hpp"
+#include "shape.hpp"
 
-class Rectangle {
+class Rectangle : public Shape
+{
 	Point ul_, br_;
 public:
 	Rectangle(Point ul, Point br)
 	: ul_(ul), br_(br)
 	{}
 
-	void print()
+	void print() const
 	{
+		std::cout << "Rectangle: ";
 		ul_.print();
 		br_.print();
 	}
---
+++ 20-11-2018-make/shape.hpp
@@ -0,0 +1,7 @@
+#include "point.hpp"
+
+class Shape
+{
+	virtual void print() const = 0;
+	virtual double area() const = 0;
+};
