2a3439c49f8c8ae9f5847062a501a02d0d328ec3
Martin_Jordanov <martistj@gmail.com>
2018-12-23 15:27:28 +0200

    [Update] First diagonal check

--- 2019-01-03-queens/queens.cxx
+++ 2019-01-03-queens/queens.cxx
@@ -12,7 +12,17 @@ bool check_position(bool **board, uint8_t x, uint8_t y, uint8_t size)
 {
 	for(uint8_t i = 0; i < size; ++i)
 	{
-		if(board[i][y] == 1 || board[x][i] == 1)
+		if(board[i][y] || board[x][i])
 			return false;
 	}
+    uint8_t diff = std::min(x, y);
+    uint8_t tmp_x = x - diff, tmp_y = y - diff;
+    while(tmp_x < size and tmp_y < size)
+    {
+        if(board[tmp_x][tmp_y])
+            return false;
+            
+        tmp_x++;
+        tmp_y++;
+    }
 }
