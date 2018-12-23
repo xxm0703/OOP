5bd4cadf1ca511ebca0e8556625660122a2a62dc
Martin_Jordanov <martistj@gmail.com>
2018-12-23 15:43:25 +0200

    [Update] All the checks

--- 2019-01-03-queens/queens.cxx
+++ 2019-01-03-queens/queens.cxx
@@ -10,19 +10,36 @@ int main(int argc, char const *argv[]) {
 
 bool check_position(bool **board, uint8_t x, uint8_t y, uint8_t size)
 {
+    // horizontal and vertical check
 	for(uint8_t i = 0; i < size; ++i)
 	{
 		if(board[i][y] || board[x][i])
 			return false;
 	}
-    uint8_t diff = std::min(x, y);
-    uint8_t tmp_x = x - diff, tmp_y = y - diff;
+
+    // Decending diagonal
+    uint8_t minimal = std::min(x, y);
+    uint8_t tmp_x = x - minimal, tmp_y = y - minimal;
     while(tmp_x < size and tmp_y < size)
     {
         if(board[tmp_x][tmp_y])
             return false;
-            
+
         tmp_x++;
         tmp_y++;
     }
+
+    // Ascending diagonal
+    minimal = std::min((uint8_t)(size - x), y);
+    tmp_x = x + minimal;
+    tmp_y = y - minimal;
+    while(tmp_x >= 0 and tmp_y < size)
+    {
+        if(board[tmp_x][tmp_y])
+            return false;
+
+        tmp_x--;
+        tmp_y++;
+    }
+    return true;
 }
