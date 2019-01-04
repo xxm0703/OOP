036121b7c529f668b42c7e2cb76536f4e37d3ee2
Martin Jordanov <xxm0703@abv.bg>
2019-01-04 22:47:32 +0200

    [Add] Hillbert curve

--- 2019-01-03-queens/queens.cxx
+++ 2019-01-03-queens/queens.cxx
@@ -2,93 +2,99 @@
 #include<cstdint>
 #include<algorithm>
 
-void solve_board(uint8_t);
-void place_queens(bool **, uint8_t, uint8_t *, uint8_t level=0);
-void print_solution(uint8_t *, uint8_t);
-bool check_position(bool **, uint8_t, uint8_t, uint8_t);
-
-int main(int argc, char const *argv[]) {
-	solve_board(atoi(argv[1]));
-	return 0;
-}
-
-void solve_board(uint8_t board_side)
+class NQueens
 {
-    if (board_side < 4)
-        return;
+	uint8_t side_;
+	uint8_t *solution_array_;
+	bool **board_;
 
-    bool **board = new bool*[board_side];
-    uint8_t *solution_array = new uint8_t[board_side];
-    for (uint8_t i = 0; i < board_side; ++i)
-        board[i] = new bool[board_side];
-
-    place_queens(board, board_side, solution_array);
-    for (uint8_t i = 0; i < board_side; ++i)
-        delete [] board[i];
-    delete [] board;
-    delete [] solution_array;
-}
-
-void place_queens(bool **board, uint8_t board_side, uint8_t *solution, uint8_t level)
-{
-    if (level == board_side)
-    {
-        print_solution(solution, board_side);
-        return;
-    }
-    for (int i = 0; i < board_side; ++i)
-    {
-
-        if(check_position(board, level, i, board_side))
-        {
-            solution[level] = i;
-            board[level][i] = true;
-            place_queens(board, board_side, solution, level + 1);
-            board[level][i] = false;
-        }
-    }
-    return;
-}
-
-void print_solution(uint8_t *positions, uint8_t size)
-{
-    for(uint8_t i = 0; i < size; ++i)
-        std::cout << (int)(positions[i]) << ' ';
-    std::cout << std::endl;
-}
-
-bool check_position(bool **board, uint8_t x, uint8_t y, uint8_t size)
-{
-    // horizontal and vertical check
-	for(uint8_t i = 0; i < size; ++i)
+	void print_solution()
 	{
-		if(board[i][y] || board[x][i])
-			return false;
+	    for(uint8_t i = 0; i < side_; ++i)
+	        std::cout << (int)(solution_array_[i]) << ' ';
+	    std::cout << std::endl;
 	}
 
-    // Decending diagonal
-    int16_t minimal = std::min(x, y);
-    int16_t tmp_x = x - minimal, tmp_y = y - minimal;
-    while(tmp_x < size && tmp_y < size)
-    {
-        if(board[tmp_x][tmp_y])
-            return false;
+	bool check_position(uint8_t x, uint8_t y)
+	{
+	    // horizontal and vertical check
+		for(uint8_t i = 0; i < side_; ++i)
+		{
+			if(board_[i][y] || board_[x][i])
+				return false;
+		}
 
-        tmp_x++;
-        tmp_y++;
-    }
+	    // Decending diagonal
+	    int16_t minimal = std::min(x, y);
+	    int16_t tmp_x = x - minimal, tmp_y = y - minimal;
+	    while(tmp_x < side_ && tmp_y < side_)
+	    {
+	        if(board_[tmp_x][tmp_y])
+	            return false;
 
-    // Ascending diagonal
-    minimal = std::min((uint8_t)(size - x - 1), y);
-    tmp_x = x + minimal;
-    tmp_y = y - minimal;
-    while(tmp_x >= 0 && tmp_y < size)
-    {
-        if(board[tmp_x][tmp_y])
-            return false;
+	        tmp_x++;
+	        tmp_y++;
+	    }
 
-        tmp_x--;
-        tmp_y++;
-    }
-    return true;
+	    // Ascending diagonal
+	    minimal = std::min((uint8_t)(side_ - x - 1), y);
+	    tmp_x = x + minimal;
+	    tmp_y = y - minimal;
+	    while(tmp_x >= 0 && tmp_y < side_)
+	    {
+	        if(board_[tmp_x][tmp_y])
+	            return false;
+
+	        tmp_x--;
+	        tmp_y++;
+	    }
+	    return true;
+	}
+
+public:
+
+	NQueens(uint8_t board_side)
+	: side_(board_side), solution_array_(new uint8_t[board_side]), board_(new bool*[board_side])
+	{
+	    for (uint8_t i = 0; i < side_; ++i)
+	        board_[i] = new bool[side_];
+	}
+
+	~NQueens()
+	{
+		for (uint8_t i = 0; i < side_; ++i)
+				delete [] board_[i];
+		
+		delete [] board_;
+		delete [] solution_array_;
+	}
+
+	void run(uint8_t level = 0)
+	{
+	    if (level == side_)
+	    {
+	        print_solution();
+	        return;
+	    }
+	    
+		for (int i = 0; i < side_; ++i)
+	    {
+
+	        if(check_position(level, i))
+	        {
+	            solution_array_[level] = i;
+	            board_[level][i] = true;
+	            run(level + 1);
+	            board_[level][i] = false;
+	        }
+	    }
+	    return;
+	}
+
+};
+
+int main(int argc, char const *argv[]) {
+	NQueens b(atoi(argv[1]));
+	b.run();
+	return 0;
 }
