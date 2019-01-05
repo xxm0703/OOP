453846ce156a988f8150c892d3d0870fe6233acf
Martin Jordanov <xxm0703@abv.bg>
2019-01-05 18:19:41 +0200

    [Update] Semi-finished version

--- 2019-01-08-sierpinski/sierpinski.cc
+++ 2019-01-08-sierpinski/sierpinski.cc
@@ -10,13 +10,13 @@ public:
 	Hilbert(Turtle& turtle)
 	: turtle_(turtle), step_(20)
 	{}
-	
+
 	void draw_a(int order);
 	void draw_b(int order);
 	void draw_c(int order);
 	void draw_d(int order);
-	
-	
+
+
 };
 
 void Hilbert::draw_a(int order) {
@@ -25,14 +25,20 @@ void Hilbert::draw_a(int order) {
 		turtle_.forward(step_);
 		return;
 	}
-	
-	turtle_.set_heading(-180).forward(step_);
-	draw_d(order-1);
-	turtle_.set_heading(90).forward(step_);
-	draw_a(order-1);
-	turtle_.set_heading(0).forward(step_);
-	draw_b(order-1);
-	turtle_.set_heading(-90).forward(step_);
+
+		turtle_.set_heading(180).forward(step_);
+		turtle_.set_heading(-135).forward(step_);
+		draw_d(order-1);
+		turtle_.set_heading(45).forward(step_);
+		turtle_.set_heading(90).forward(step_);
+		turtle_.set_heading(135).forward(step_);
+		draw_a(order-1);
+		turtle_.set_heading(-45).forward(step_);
+		turtle_.set_heading(0).forward(step_);
+		turtle_.set_heading(45).forward(step_);
+		draw_b(order-1);
+		turtle_.set_heading(-135).forward(step_);
+		turtle_.set_heading(-90).forward(step_);
 }
 
 void Hilbert::draw_b(int order) {
@@ -42,30 +48,42 @@ void Hilbert::draw_b(int order) {
 		turtle_.forward(step_);
 		return;
 	}
-	
+
 	turtle_.set_heading(90).forward(step_);
+	turtle_.set_heading(135).forward(step_);
 	draw_a(order-1);
+	turtle_.set_heading(-45).forward(step_);
 	turtle_.set_heading(0).forward(step_);
+	turtle_.set_heading(45).forward(step_);
 	draw_b(order-1);
+	turtle_.set_heading(-135).forward(step_);
 	turtle_.set_heading(-90).forward(step_);
+	turtle_.set_heading(-45).forward(step_);
 	draw_c(order-1);
+	turtle_.set_heading(135).forward(step_);
 	turtle_.set_heading(180).forward(step_);
 }
 
 void Hilbert::draw_c(int order) {
-	
+
 	if(order <= 0) {
 		turtle_.set_heading(-135);
 		turtle_.forward(step_);
 		return;
 	}
-	
+
 	turtle_.set_heading(0).forward(step_);
+	turtle_.set_heading(45).forward(step_);
 	draw_b(order-1);
+	turtle_.set_heading(-135).forward(step_);
 	turtle_.set_heading(-90).forward(step_);
+	turtle_.set_heading(-45).forward(step_);
 	draw_c(order-1);
+	turtle_.set_heading(135).forward(step_);
 	turtle_.set_heading(180).forward(step_);
+	turtle_.set_heading(-135).forward(step_);
 	draw_d(order-1);
+	turtle_.set_heading(45).forward(step_);
 	turtle_.set_heading(90).forward(step_);
 }
 void Hilbert::draw_d(int order) {
@@ -75,13 +93,19 @@ void Hilbert::draw_d(int order) {
 		turtle_.forward(step_);
 		return;
 	}
-	
-	turtle_.set_heading(90).forward(step_);
-	draw_c(order-1);
-	turtle_.set_heading(180).forward(step_);
-	draw_d(order-1);
+
 	turtle_.set_heading(-90).forward(step_);
+	turtle_.set_heading(-45).forward(step_);
+	draw_c(order-1);
+	turtle_.set_heading(135).forward(step_);
+	turtle_.set_heading(180).forward(step_);
+	turtle_.set_heading(-135).forward(step_);
+	draw_d(order-1);
+	turtle_.set_heading(45).forward(step_);
+	turtle_.set_heading(90).forward(step_);
+	turtle_.set_heading(135).forward(step_);
 	draw_a(order-1);
+	turtle_.set_heading(-45).forward(step_);
 	turtle_.set_heading(0).forward(step_);
 }
 
@@ -90,9 +114,12 @@ int main() {
 	PSTurtle t = PSTurtle(300, 300);
 	t.setup().moveto(Point(150,150));
 	t.pendown();
-	
+
 	Hilbert h(t);
-	h.draw_d(3);
+	h.draw_a(3);
+	// h.draw_b(1);
+	// h.draw_c(1);
+	// h.draw_d(1);
 
 
 	return 0;
