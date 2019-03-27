d7bc60a547f5434a729777ff9009dec57dbb5329
Martin_Jordanov <martistj@gmail.com>
2019-03-28 01:20:44 +0200

    [Add] GameFactory implementation
    Signed-off-by: Martin_Jordanov <martistj@gmail.com>

--- 2019-04-14-homework/src/org/elsys/cardgame/factory/GameFactory.java
+++ 2019-04-14-homework/src/org/elsys/cardgame/factory/GameFactory.java
@@ -1,21 +1,56 @@
 package org.elsys.cardgame.factory;
 
 import org.elsys.cardgame.api.Card;
+import org.elsys.cardgame.api.CardException;
+import org.elsys.cardgame.api.Deck;
 import org.elsys.cardgame.api.Game;
+import org.elsys.cardgame.factory.operations.*;
 
 import java.util.List;
 
 public class GameFactory {
 
     public static Game createWarGame(List<Card> cards) {
-        return new GameImpl(DeckFactory.defaultWarDeck(), null);
+        final Deck defaultWarDeck = DeckFactory.defaultWarDeck();
+
+        if (cards.containsAll(defaultWarDeck.getCards())) {
+            final GameImpl game = new GameImpl(defaultWarDeck, null);
+            loadBasicOperations(game);
+            return game;
+        }
+        throw new CardException("ERROR: Not enough cards for War");
     }
 
     public static Game createSantaseGame(List<Card> cards) {
-        return new GameImpl(DeckFactory.defaultSantaseDeck(), null);
+        final Deck defaultSantaseDeck = DeckFactory.defaultSantaseDeck();
+
+        if (cards.containsAll(defaultSantaseDeck.getCards())) {
+            final GameImpl game = new GameImpl(defaultSantaseDeck, null);
+            loadBasicOperations(game);
+            return game;
+        }
+        throw new CardException("ERROR: Not enough cards for Santase");
     }
 
     public static Game createBeloteGame(List<Card> cards) {
-        return new GameImpl(DeckFactory.defaultBeloteDeck(), null);
+        final Deck defaultBeloteDeck = DeckFactory.defaultBeloteDeck();
+
+        if (cards.containsAll(defaultBeloteDeck.getCards())) {
+            final GameImpl game = new GameImpl(defaultBeloteDeck, null);
+            loadBasicOperations(game);
+            return game;
+        }
+        throw new CardException("ERROR: Not enough cards for Belote");
+    }
+
+    private static void loadBasicOperations(final Game game) {
+        game.addOperation(new Size("size", game));
+        game.addOperation(new DrawTopCard("draw_top_card", game));
+        game.addOperation(new DrawBottomCard("draw_bottom_card", game));
+        game.addOperation(new PeekTopCard("top_card", game));
+        game.addOperation(new PeekBottomCard("bottom_card", game));
+        game.addOperation(new Shuffle("shuffle", game));
+        game.addOperation(new Sort("sort", game));
+        game.addOperation(new Deal("deal", game));
     }
 }
